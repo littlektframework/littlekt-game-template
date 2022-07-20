1f0def0f1c570f42d2f781926c836655a9a98b55
LeHaine <colt.daily@gmail.com>
2022-07-20 11:58:48 -0400

    littlekt: update to version 0.3.0
    kotlin: update to version 1.7.0

--- game/src/commonMain/kotlin/com/game/template/Game.kt
+++ game/src/commonMain/kotlin/com/game/template/Game.kt
@@ -4,24 +4,39 @@ import com.lehaine.littlekt.Context
 import com.lehaine.littlekt.ContextListener
 import com.lehaine.littlekt.graphics.*
 import com.lehaine.littlekt.graphics.gl.ClearBufferMask
+import com.lehaine.littlekt.graphics.shape.ShapeRenderer
+import com.lehaine.littlekt.math.geom.degrees
+import com.lehaine.littlekt.math.geom.radians
+import com.lehaine.littlekt.util.viewport.ExtendViewport
+import kotlin.time.Duration.Companion.milliseconds
 
 
 class Game(context: Context) : ContextListener(context) {
 
     override suspend fun Context.start() {
         val batch = SpriteBatch(this)
-        val camera = OrthographicCamera(graphics.width, graphics.height)
+        val shapeRenderer = ShapeRenderer(batch)
+        val viewport = ExtendViewport(960, 540)
+        val camera = viewport.camera
+        var rotation = 0.radians
+        var rotationTimer = 0.milliseconds
 
         onResize { width, height ->
-            camera.update(width, height, context)
+            viewport.update(width, height, context)
         }
 
-        onRender {
+        onRender { dt ->
             gl.clearColor(Color.DARK_GRAY)
             gl.clear(ClearBufferMask.COLOR_BUFFER_BIT)
 
             batch.use(camera.viewProjection) {
                 Fonts.default.draw(it, "Hello LittleKt!", 0f, 0f)
+                shapeRenderer.filledRectangle(-50f, 50f, 100f, 50f, rotation, color = Color.RED.toFloatBits())
+            }
+            rotationTimer += dt
+            if (rotationTimer > 10.milliseconds) {
+                rotationTimer = 0.milliseconds
+                rotation += 1.degrees
             }
         }
     }
